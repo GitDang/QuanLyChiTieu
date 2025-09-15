53008d21dabbf5b6c19e3662f11bc15e57827f1a
dangtm1 <minhdangit95@gmail.com>
2025-09-15 16:04:47 +0900

    update sdk 35

--- quan_ly_chi_tieu/macos/Flutter/GeneratedPluginRegistrant.swift
+++ quan_ly_chi_tieu/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import file_selector_macos
 import in_app_purchase_storekit
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
